4a1063b2e86533f4233b50a921cddffe99c1facd
Asil <asyard@gmail.com>
2022-07-26 16:20:20 +0300

    reformatting structure

---
+++ pkg/cons.go
@@ -0,0 +1,10 @@
+package pkg
+
+const (
+	GsscApiHeadersToday         = "https://rerererarara.net/api/v1/basliklar/bugun"
+	GsscApiEntriesOfHeaderToday = "https://rerererarara.net/api/v1/entryler/birgun?baslik_id="
+	GsscApiHeadersAgenda        = "https://rerererarara.net/api/v1/basliklar/gundem/basliklar"
+	GsscApiEntryDetail          = "https://rerererarara.net/api/v1/entry/detay?entry_id="
+	GsscApiProfileDetail        = "https://rerererarara.net/api/v1/kullanici/profil?nick="
+	GsscApiProfileStatsDetail   = "https://rerererarara.net/api/v1/kullanici/liste?nick="
+)
